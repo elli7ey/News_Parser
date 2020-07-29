037b6992787b6530de3d75f1e92030988c0f6f5b
AndreyeuIvan <andreeviv@icloud.com>
2020-07-29 20:50:10 +0300

    created module, admin,created first newspost

--- config/news/admin.py
+++ config/news/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import News
 
-# Register your models here.
+
+@admin.register(News)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author','status')
+    list_filter = ('status', 'created', 'updated', 'author')
+    search_fields = ('title', 'body')
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('author',)
+    date_hierarchy = 'updated'
+    ordering = ('status', 'updated')
